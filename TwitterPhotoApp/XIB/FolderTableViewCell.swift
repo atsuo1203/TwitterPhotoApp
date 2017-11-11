a1c7e919e169e8e997c8982d84d17fe40f6fa249
atsuo1203 <dondonkitemasu@gmail.com>
2017-11-12 03:55:55 +0900

    Folder.xib

---
+++ TwitterPhotoApp/XIB/FolderTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FolderTableViewCell.swift
+//  TwitterPhotoApp
+//
+//  Created by Atsuo Yonehara on 2017/11/12.
+//  Copyright © 2017年 Atsuo Yonehara. All rights reserved.
+//
+
+import UIKit
+
+class FolderTableViewCell: UITableViewCell {
+    @IBOutlet weak var folderLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
