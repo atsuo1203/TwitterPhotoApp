66c64af80da44f1251ec5d86ee9d8dae3629e1db
atsuo1203 <dondonkitemasu@gmail.com>
2017-11-12 17:24:50 +0900

    一応取得はできた

--- TwitterPhotoApp/DetailViewController.swift
+++ TwitterPhotoApp/DetailViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import TwitterKit
 
 class DetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         print(self.navigationItem.title!)
+        getSantaGirls()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,15 +22,28 @@ class DetailViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func getSantaGirls() {
+        let client = TWTRAPIClient()
+        let statusesShowEndpoint = "https://api.twitter.com/1.1/search/tweets.json"
+        let params = [
+            "q": "#サンタコス -割 -Set -メイド風 -アクセント -点セット -お買い得 -#子供 -#マンチカン -#サンタ衣装 -#コスチューム -#sugar filter:images exclude:retweets ",
+            "lang": "ja",
+            "count": "1",
+            ]
+        var clientError : NSError?
+        let request = client.urlRequest(withMethod: "GET", url: statusesShowEndpoint, parameters: params, error: &clientError)
+        client.sendTwitterRequest(request) { (response, data, connectionError) -> Void in
+            if connectionError != nil {
+                print("Error: \(String(describing: connectionError))")
+            }
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data!, options: [])
+                print("json: \(json)")
+            } catch let jsonError as NSError {
+                print("json error: \(jsonError.localizedDescription)")
+            }
+        }
     }
-    */
 
 }
